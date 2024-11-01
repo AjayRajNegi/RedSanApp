96caf695ec17eff3e576b738974d46a3e96378e7
Ajay <ajayrajnegi111@gmail.com>
2024-11-01 12:40:37 +0530

    accept current changes

--- src/screens/Auth/SignUpScreen.tsx
+++ src/screens/Auth/SignUpScreen.tsx
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
 import {View, Text, StyleSheet} from 'react-native';
-=======
-import {View, Text} from 'react-native';
->>>>>>> 3e9cfdc2814947a855c65f58deac5841b875d7de
 import React from 'react';
 
 const SignUpScreen = () => {
   return (
     <View>
-<<<<<<< HEAD
       <Text style={styles.text}>SignUpScreen</Text>
-=======
-      <Text>SignUpScreenText</Text>
->>>>>>> 3e9cfdc2814947a855c65f58deac5841b875d7de
     </View>
   );
 };
 
 export default SignUpScreen;
-<<<<<<< HEAD
 
 const styles = StyleSheet.create({
   text: {color: 'black'},
 });
-=======
->>>>>>> 3e9cfdc2814947a855c65f58deac5841b875d7de
